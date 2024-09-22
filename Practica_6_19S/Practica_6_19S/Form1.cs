e04fcc6b727fbd2666d91de55235e6820e34120c
Hamza Derouich <hderouiche01@educarex.es>
2024-09-22 20:48:32 +0200

    Nuevos Ejercicios

--- Practica_6_19S/Practica_6_19S/Form1.cs
+++ Practica_6_19S/Practica_6_19S/Form1.cs
@@ -3,9 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -14,212 +12,222 @@ namespace Practica_6_19S
 {
     public partial class Form1 : Form
     {
+        
         Random aleatorios = new Random();
-        System.Windows.Forms.Timer tmrCuentaJuego = new System.Windows.Forms.Timer();
-        System.Windows.Forms.Timer tmrOcultarBotones = new System.Windows.Forms.Timer();
-        int contadorErros = 0;
-        int contadorOcultarBotnes = 5;
-        int contadorTiempoJuego = 10;
+        System.Windows.Forms.Timer timmerOcultar = new System.Windows.Forms.Timer();
+        System.Windows.Forms.Timer timmerTiempoPartida = new System.Windows.Forms.Timer();
+        System.Windows.Forms.Timer timmerOcultarImagen = new System.Windows.Forms.Timer();
 
-       static String[] imagenes = {
 
-              "C:\\Users\\Usuario\\source\\repos\\Practica_6_19S\\Practica_6_19S\\imagenes\\0.JPG",
-              "C:\\Users\\Usuario\\source\\repos\\Practica_6_19S\\Practica_6_19S\\imagenes\\1.JPG",
-              "C:\\Users\\Usuario\\source\\repos\\Practica_6_19S\\Practica_6_19S\\imagenes\\2.JPG",
-              "C:\\Users\\Usuario\\source\\repos\\Practica_6_19S\\Practica_6_19S\\imagenes\\3.JPG",
-              "C:\\Users\\Usuario\\source\\repos\\Practica_6_19S\\Practica_6_19S\\imagenes\\4.JPG",
-              "C:\\Users\\Usuario\\source\\repos\\Practica_6_19S\\Practica_6_19S\\imagenes\\5.JPG",
-              "C:\\Users\\Usuario\\source\\repos\\Practica_6_19S\\Practica_6_19S\\imagenes\\6.JPG",
-              "C:\\Users\\Usuario\\source\\repos\\Practica_6_19S\\Practica_6_19S\\imagenes\\7.JPG",
-              "C:\\Users\\Usuario\\source\\repos\\Practica_6_19S\\Practica_6_19S\\imagenes\\8.JPG",
-              "C:\\Users\\Usuario\\source\\repos\\Practica_6_19S\\Practica_6_19S\\imagenes\\9.JPG",
-              "C:\\Users\\Usuario\\source\\repos\\Practica_6_19S\\Practica_6_19S\\imagenes\\10.JPG",
-              "C:\\Users\\Usuario\\source\\repos\\Practica_6_19S\\Practica_6_19S\\imagenes\\11.JPG",
-              "C:\\Users\\Usuario\\source\\repos\\Practica_6_19S\\Practica_6_19S\\imagenes\\12.JPG",
-              "C:\\Users\\Usuario\\source\\repos\\Practica_6_19S\\Practica_6_19S\\imagenes\\13.JPG",
-              "C:\\Users\\Usuario\\source\\repos\\Practica_6_19S\\Practica_6_19S\\imagenes\\14.JPG",
-              "C:\\Users\\Usuario\\source\\repos\\Practica_6_19S\\Practica_6_19S\\imagenes\\15.JPG",
-              "C:\\Users\\Usuario\\source\\repos\\Practica_6_19S\\Practica_6_19S\\imagenes\\16.JPG",
-              "C:\\Users\\Usuario\\source\\repos\\Practica_6_19S\\Practica_6_19S\\imagenes\\17.JPG",
-              "C:\\Users\\Usuario\\source\\repos\\Practica_6_19S\\Practica_6_19S\\imagenes\\18.JPG",
-              "C:\\Users\\Usuario\\source\\repos\\Practica_6_19S\\Practica_6_19S\\imagenes\\19.JPG",
+        int contadorOcultar = 5;
+        int contadorTiempoPartida = 10;
+        int contadorAciertos = 0;
 
-            };
+        static string[] imagenes =
+        {
+            "C:\\Users\\Usuario\\source\\repos\\Practica_6_19S\\Practica_6_19S\\imagenes\\0.JPG",
+            "C:\\Users\\Usuario\\source\\repos\\Practica_6_19S\\Practica_6_19S\\imagenes\\1.JPG",
+            "C:\\Users\\Usuario\\source\\repos\\Practica_6_19S\\Practica_6_19S\\imagenes\\2.JPG",
+            "C:\\Users\\Usuario\\source\\repos\\Practica_6_19S\\Practica_6_19S\\imagenes\\3.JPG",
+            "C:\\Users\\Usuario\\source\\repos\\Practica_6_19S\\Practica_6_19S\\imagenes\\4.JPG",
+            "C:\\Users\\Usuario\\source\\repos\\Practica_6_19S\\Practica_6_19S\\imagenes\\5.JPG",
+            "C:\\Users\\Usuario\\source\\repos\\Practica_6_19S\\Practica_6_19S\\imagenes\\6.JPG",
+            "C:\\Users\\Usuario\\source\\repos\\Practica_6_19S\\Practica_6_19S\\imagenes\\7.JPG",
+            "C:\\Users\\Usuario\\source\\repos\\Practica_6_19S\\Practica_6_19S\\imagenes\\8.JPG",
+            "C:\\Users\\Usuario\\source\\repos\\Practica_6_19S\\Practica_6_19S\\imagenes\\9.JPG",
+            "C:\\Users\\Usuario\\source\\repos\\Practica_6_19S\\Practica_6_19S\\imagenes\\10.JPG",
+            "C:\\Users\\Usuario\\source\\repos\\Practica_6_19S\\Practica_6_19S\\imagenes\\11.JPG",
+            "C:\\Users\\Usuario\\source\\repos\\Practica_6_19S\\Practica_6_19S\\imagenes\\12.JPG",
+            "C:\\Users\\Usuario\\source\\repos\\Practica_6_19S\\Practica_6_19S\\imagenes\\13.JPG",
+            "C:\\Users\\Usuario\\source\\repos\\Practica_6_19S\\Practica_6_19S\\imagenes\\14.JPG",
+            "C:\\Users\\Usuario\\source\\repos\\Practica_6_19S\\Practica_6_19S\\imagenes\\15.JPG",
+            "C:\\Users\\Usuario\\source\\repos\\Practica_6_19S\\Practica_6_19S\\imagenes\\16.JPG",
+            "C:\\Users\\Usuario\\source\\repos\\Practica_6_19S\\Practica_6_19S\\imagenes\\17.JPG",
+            "C:\\Users\\Usuario\\source\\repos\\Practica_6_19S\\Practica_6_19S\\imagenes\\18.JPG",
+            "C:\\Users\\Usuario\\source\\repos\\Practica_6_19S\\Practica_6_19S\\imagenes\\19.JPG",
+        };
 
         public Form1()
         {
             InitializeComponent();
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            // Metodos 
-
             CargarImagenes();
-            ConfigurarTmrOcultarBotnes();
-            ConfigurarTmrCuentaJuego();
+            ConfOcultarBotones();
+            ConfiguracionTimerTiempoPartida();
+            ConfOcultarImagen();
+            
+        }
 
-            // Eventos 
+        // Metodo que configura el timmer del timpo de partidad 
 
-            tmrOcultarBotones.Tick += TmrOcultarBotones_Tick;
-            tmrCuentaJuego.Tick += TmrCuentaJuego_Tick;
+        private void ConfiguracionTimerTiempoPartida()
+        {
+            timmerTiempoPartida.Tick -= TimmerTiempoPartida_Tick;
+            timmerTiempoPartida.Tick += TimmerTiempoPartida_Tick;
+            timmerTiempoPartida.Enabled = false;
+            timmerTiempoPartida.Interval = 1000;
+        }
 
-            // Cambiar color pantalla
+        // Timmer del tiempo de partida. Algoritmo que controla la Actualización y la generación de imagenes y
+        // label de la cuenta de atrás.
 
-            switch (contadorTiempoJuego)
+        private void TimmerTiempoPartida_Tick(object sender, EventArgs e)
+        {
+            timmerOcultarImagen.Start();
+            label2.Text = contadorTiempoPartida.ToString();
+            contadorTiempoPartida--;
+            if (contadorTiempoPartida < 0)
             {
-                case 5:
-                    this.BackColor = Color.Yellow;
-                    break;
-                case 4:
-                case 3:
-                case 2:
-                    this.BackColor = Color.Red;
-                    break;
-                case 1:
-                    this.BackColor = Color.Black;
-                    break;
-
-
+                timmerOcultarImagen.Stop();
+                timmerTiempoPartida.Stop();
+                timmerOcultar.Start();
+                GenerarNuevasImagenes();
+                contadorTiempoPartida = 10;
             }
         }
 
-        private void TmrCuentaJuego_Tick(object sender, EventArgs e)
+        // Metodo asociado al timmer de tiempo de partida encargado de generar nuevas imagenes 
+
+        private void GenerarNuevasImagenes()
         {
-
-            label2.Text = contadorTiempoJuego.ToString();
-            contadorTiempoJuego--;
-            if (contadorTiempoJuego == -1)
-            {
-                contadorTiempoJuego = 10;
-                tmrCuentaJuego.Enabled = false;
-
-                // Metodo colores pantalla 
-
-                
-
-                // Generamos una nueva imagen aleatoria
-
-                button1.Image = Image.FromFile(imagenes[aleatorios.Next(0, imagenes.Length)]);
-
-                // Generar nuevas imagenes
-
-                CargarImagenes() ;
-
-                // Habilitamos el Timmer para ocultar los botones 
-
-                tmrOcultarBotones.Enabled = true;
-            }
-
-        }
-
-       
-
-        // Metodo que muestra el contenido de los botones y realiza la la comprobacion de si las imagenes son iguales
-        //Atascado preguntar a camacho 
-        private void MostrarContenidoBoton()
-        {
+            String rutaImagenAveriguar = imagenes[aleatorios.Next(0, imagenes.Length)];
+            button1.Image = Image.FromFile(rutaImagenAveriguar);
+            button1.Tag = rutaImagenAveriguar;
             foreach (Control control in flowLayoutPanel2.Controls)
             {
-
-            }
-        }
-
-
-        // Metodo que configura el timmer del tiempo de juego
-        private void ConfigurarTmrCuentaJuego()
-        {
-         
-            tmrCuentaJuego.Enabled = false;
-            tmrCuentaJuego.Interval = 1000;
-            
-        }
-
-        // Metodo que configura el timmer de ocultar los botones 
-
-        private void TmrOcultarBotones_Tick(object sender, EventArgs e)
-        {
-            
-            label1.Text = contadorOcultarBotnes.ToString();
-            contadorOcultarBotnes--;
-            if(contadorOcultarBotnes == -1)
-            {
-                contadorOcultarBotnes = 5;
-                tmrOcultarBotones.Enabled = false;
-                tmrCuentaJuego.Enabled = true ;
-
-                // Metodo Ocultar botones
-
-                OcultarBotones();
-
-            }
-        }
-
-        // Metodo que oculta los botones 
-        private void OcultarBotones()
-        {
-            
-            foreach ( Control control in flowLayoutPanel2.Controls )
-            {
-                if ( control is Button btn )
+                if (control is Button button)
                 {
-
-                    btn.Enabled = true;
-                    btn.Image = Image.FromFile("C:\\Users\\Usuario\\source\\repos\\Practica_6_19S\\Practica_6_19S\\imagenes\\manzana.jpg");
-                    
+                    String rutaImagen = imagenes[aleatorios.Next(0, imagenes.Length)];
+                    Image imagen = Image.FromFile(rutaImagen);
+                    button.Image = imagen;
+                    button.Tag = rutaImagen;
+                    button.Enabled = false;
                 }
             }
-            
         }
 
-        // Metodo que configura el timmer que oculta los botones 
-        private void ConfigurarTmrOcultarBotnes()
+        // Evento button click ( el evento se genera en el metodo cargar imagenes ) , se encarga de comprobar los aciertos y los errores del usuario 
+        
+        private void Button_Click(object sender, EventArgs e)
         {
-           tmrOcultarBotones.Enabled = true;
-           tmrOcultarBotones.Interval = 1000;
+            label3.Text = contadorAciertos.ToString();
+            Button button = (Button)sender;
+            string rutaImagen = button.Tag.ToString();
+            button.Image = Image.FromFile(rutaImagen);
+            button.AutoSize = true;
+            if (button.Tag.Equals(button1.Tag))
+            {
+               
+                contadorAciertos++;
+            }
+            else
+            {
+                contadorAciertos--;
+            }
         }
+
+        // Metodo de encargar los botones con las imagenes ( las rutas de las imagenes se encuentrar en el contenedor de imagenes) 
 
         private void CargarImagenes()
         {
-            // Los botones van a comenzar deshabilitados 
+            ConfiguracionButtonImagenAveriguar();
+
+            for (int i = 0; i < 42; i++)
+            {
+                CrearButtonImageFlPanel();
+
+            }
+        }
+
+        // Configuracion de los botones que se crean en el metodo cargar imagenes
+        // La asiganacion de la imgaen  y sus eventos 
+
+        private void CrearButtonImageFlPanel()
+        {
+            Button button = new Button();
+            String rutaimagen = imagenes[aleatorios.Next(0, imagenes.Length)];
+            button.Image = Image.FromFile(rutaimagen);
+            button.Tag = rutaimagen;
+            button.AutoSize = true;
+            button.Enabled = false;
+            button.Click += Button_Click;
+            flowLayoutPanel2.Controls.Add(button);
+        }
+
+        // Configuracion del timmer ocultar imagenes encargado de ocultar las imagenes 
+        // En el se asignan sus parametros y sus respectivos eventos 
 
 
-            // Generamos la imagen que el usuario tiene que adivinar 
+        private void ConfOcultarImagen()
+        {
+            timmerOcultarImagen.Tick -= TimmerOcultarImagen_Tick;
+            timmerOcultarImagen.Tick += TimmerOcultarImagen_Tick;
+            timmerOcultarImagen.Enabled = false;
+            timmerOcultarImagen.Interval = 500;
+        }
 
-            button1.Image = Image.FromFile(imagenes[aleatorios.Next(0, imagenes.Length)]);
+        // Timmer ocultar imagen que lleva asociado un metodo encargado de ocultar los botones
+
+        private void TimmerOcultarImagen_Tick(object sender, EventArgs e)
+        {
+            Ocultarbotones();
+        }
+
+        // Configuracion del button de la imagen que tiene que averiguar el usuario
+        // En él se asigan sus parametros y la imagén 
+
+        private void ConfiguracionButtonImagenAveriguar()
+        {
+            String rutaimagen = imagenes[aleatorios.Next(0, imagenes.Length)];
+            button1.Image = Image.FromFile(rutaimagen);
+            button1.Tag = rutaimagen;
+            button1.Text = "";
+            button1.AutoSize = true;
+        }
 
 
-            foreach (Control control in flowLayoutPanel2.Controls)
-            { 
-                if (control is Button btn)
+        // Configuracion del timmer ocultar los botones ( Sobreponer la imagen de la manzana ) imagenes encargado de ocultar las imagenes 
+        // En el se asignan sus parametros y sus respectivos eventos 
+
+        private void ConfOcultarBotones()
+        {
+            timmerOcultar.Tick -= TimmerOcultar_Tick;
+            timmerOcultar.Tick += TimmerOcultar_Tick;
+            timmerOcultar.Enabled = true;
+            timmerOcultar.Interval = 1000;
+        }
+
+
+        // Evento encargado de controlar el timmer del tiempo que tiene el usuario para visualizar las imagenes 
+        private void TimmerOcultar_Tick(object sender, EventArgs e)
+        { 
+            timmerOcultar.Start();
+            label1.Text = contadorOcultar.ToString();
+            contadorOcultar--;
+            if (contadorOcultar < 0)
+            {
+                timmerTiempoPartida.Start();
+                timmerOcultar.Stop();
+                contadorOcultar = 5;
+            }
+
+        }
+
+        // Metodo Ocultar botones encargado de ocultar las imagenes ( sobreponer la manzana ) 
+
+        private void Ocultarbotones()
+        {
+            foreach (Control  control in flowLayoutPanel2.Controls)
+            {
+                if( control is Button button)
                 {
-                    btn.Image = Image.FromFile(imagenes[aleatorios.Next(0, imagenes.Length)]);
-                    btn.Enabled = true;
+
+                    button.Image = Image.FromFile("C:\\Users\\Usuario\\source\\repos\\Practica_6_19S\\Practica_6_19S\\imagenes\\manzana.JPG");
+                    button.AutoSize = true;
+                    button.Enabled = true;
+                    
                 }
             }
-        
-        }
-
-        // Metodos click 
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            
-
-           
-        }
-
-        private void button98_Click(object sender, EventArgs e)
-        {
-            Button boton = (Button)sender;
-
-            // Mostrar la imagen actual del botón
-            boton.Image = Image.FromFile(imagenes[aleatorios.Next(0, imagenes.Length)]);
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-
         }
     }
+
 }
